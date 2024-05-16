6475d6007c17b83355250124ccbeeb8c5e57bb8d
Gustavo Pagano <gustavopagano03@gmail.com>
2024-05-16 23:15:53 +0200

    added descriptions to JS functions in Index.js

--- 01-Expanding cards/index.js
+++ 01-Expanding cards/index.js
@@ -1,13 +1,16 @@
-//creo el objeto panels, el cual contiene todos los elementos con al clase "panel"
+// I create the object panels, which contains all the elements with the class ‘panel’.
 const panels = document.querySelectorAll(".panel");
 
+// Function to add the word ‘active’ to classList, so that it applies the corresponding css formatting, generating the desired effect.
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
     panel.classList.add("active");
-    console.log("panels", panels);
+    // console.log("panels", panels);
   });
 });
+
+// Function to remove the word "active" of class panel.
 const removeActiveClasses = () => {
   panels.forEach((panel) => {
     panel.classList.remove("active");
